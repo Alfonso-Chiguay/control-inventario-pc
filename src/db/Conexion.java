aa8ec07bcd531f86d4549b17b51d612ffb7cb34a
alfonso.chiguay <alfonso.chiguay@gmail.com>
2022-03-10 10:06:43 -0300

    Ocultar credenciales

--- src/db/Conexion.java
+++ src/db/Conexion.java
@@ -15,11 +15,11 @@ import java.util.Properties;
  * @author alfon
  */
 public class Conexion {
-    private final static String SERVER="www.piliscoffee.cl:3306";
-    private final static String USER="piliscof_achiguay";
-    private final static String PASS="LaLo14!!";
-    //private final static String DB="piliscof_posdb";
-    private final static String DB="piliscof_posdb_desarrollo";
+    private final static String SERVER="######";
+    private final static String USER="######";
+    private final static String PASS="######";
+    //private final static String DB="######";
+    private final static String DB="######";
     public Connection getConnection(){
         
         Properties propiedadesConexion = new Properties();
