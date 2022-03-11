6a861fb6cf0e42775d668f55262d06284c8b543d
alfonso.chiguay <alfonso.chiguay@gmail.com>
2022-03-11 17:57:54 -0300

    avances en punto de venta conectado a la BD

--- src/controlador/ConVenta.java
+++ src/controlador/ConVenta.java
@@ -2,6 +2,7 @@ package controlador;
 
 import db.Conexion;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import javax.swing.JFrame;
@@ -20,8 +21,7 @@ public class ConVenta {
             String query = "INSERT INTO VENTA VALUES((SELECT nuevo_folio FROM SEQ_FOLIO), "
                             + "(SELECT NOW() FROM DUAL), '"
                             + venta.getTipo_venta() +"',"
-                            + venta.getTotal()+ ");";
-                    
+                            + venta.getTotal()+ ");";                   
             
             stmt.executeUpdate(query);
             
@@ -31,6 +31,9 @@ public class ConVenta {
                         + "(SELECT nuevo_folio FROM SEQ_FOLIO), "
                         + vp.getCantidad() + ","
                         + vp.getTotal()+ " );";
+                stmt.executeUpdate(query);
+                
+                query = "UPDATE PRODUCTO SET stock = stock - " + vp.getCantidad() + " WHERE codigo_barra = '" + vp.getCodigo_barra() + "';";
                 stmt.executeUpdate(query);
             }
             
@@ -43,6 +46,31 @@ public class ConVenta {
         catch(Exception e){
             JOptionPane.showMessageDialog(ventana, "No se pudo ingresar la venta", "Venta erronea", JOptionPane.INFORMATION_MESSAGE);
             return false;
+        }
+    }
+    
+    public void stockNegativo(ArrayList<VentaProducto> detalleVenta, JFrame ventana){
+        try{
+            
+            ArrayList<String> bajoStockList = new ArrayList<>();
+            Statement stmt = CONNECTION.createStatement();
+            for(VentaProducto vp: detalleVenta){
+                String query = "SELECT nombre, stock FROM PRODUCTO WHERE codigo_barra = '" + vp.getCodigo_barra() + "'";
+                ResultSet rs = stmt.executeQuery(query);
+                int stock = rs.getInt(2);
+                if(stock <= 0){
+                    JOptionPane.showMessageDialog(ventana, rs.getString(1)+" quedó con stock: "+String.valueOf(rs.getInt(2)), "Producto con problema de Stock!", JOptionPane.INFORMATION_MESSAGE);
+                    bajoStockList.add(rs.getString(1)+" = "+String.valueOf(rs.getInt(2)));
+                }                
+            }
+            
+            
+            
+            
+            
+        }
+        catch(Exception e){
+            
         }
         
         
