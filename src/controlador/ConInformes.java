28f637e9a39c3aa6cb1c347f90c81f864aa0804c
A Chiguay <alfonso.chiguay@gmail.com>
2022-03-20 16:21:05 -0300

    arreglos en informe de stock negativo. Informe de venta diaria terminado

--- src/controlador/ConInformes.java
+++ src/controlador/ConInformes.java
@@ -26,7 +26,15 @@ public class ConInformes {
 
             DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
             String today = formatter.format(date);
-            String ruta = System.getProperty("user.home")+"/Desktop/Reportes/Stock negativo/Informe stock negativo "+today+".pdf";
+                        
+            formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+            
+            String fullDate = formatter.format(date);
+            formatter = new SimpleDateFormat("HHmmss");
+            String hora = formatter.format(date);
+            
+            
+            String ruta = System.getProperty("user.home")+"/Desktop/Reportes/Stock negativo/Informe stock negativo "+today+" "+hora+".pdf";
             PdfWriter.getInstance(documento, new FileOutputStream(ruta));
             documento.open();
             Image header = Image.getInstance("src/img/Piliscoffee header.png");
@@ -42,7 +50,7 @@ public class ConInformes {
             Paragraph subtitulo = new Paragraph();
             subtitulo.setAlignment(Chunk.ALIGN_CENTER);
             subtitulo.setFont(FontFactory.getFont(FontFactory.TIMES_ROMAN, 12, Font.NORMAL, BaseColor.RED));
-            subtitulo.add("Fecha de creacion del reporte: "+today+"\n\n");
+            subtitulo.add("Fecha de creacion del reporte: "+fullDate+"\n\n");
             
             documento.open();
             documento.add(header);
@@ -103,13 +111,23 @@ public class ConInformes {
     
     public void informeVentaDiario(String fecha){
         ConVenta cVenta = new ConVenta();
+        ////////////////////
         ArrayList<Venta> listado = cVenta.ventasDia(fecha);
+        ////////////////////
         Document documento = new Document();
         try {
             Date date = Calendar.getInstance().getTime();
             DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
             String today = formatter.format(date);
-            String fechaSolicitada = fecha.split("-")[2]+"-"+fecha.split("-")[1]+"-"+fecha.split("-")[0];
+            
+            formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");            
+            String fullDate = formatter.format(date);
+            
+            formatter = new SimpleDateFormat("HHmmss");
+            String hora = formatter.format(date);
+            
+            
+            String fechaSolicitada = fecha.split("-")[2]+"-"+fecha.split("-")[1]+"-"+fecha.split("-")[0]+" "+hora;
             String ruta = System.getProperty("user.home")+"/Desktop/Reportes/Venta diaria/Venta diaria "+fechaSolicitada+".pdf";
             PdfWriter.getInstance(documento, new FileOutputStream(ruta));
             documento.open();
@@ -126,7 +144,7 @@ public class ConInformes {
             Paragraph subtitulo = new Paragraph();
             subtitulo.setAlignment(Chunk.ALIGN_CENTER);
             subtitulo.setFont(FontFactory.getFont(FontFactory.TIMES_ROMAN, 12, Font.NORMAL, BaseColor.RED));
-            subtitulo.add("Fecha de creacion del reporte: "+today+"\n\n");
+            subtitulo.add("Fecha de creacion del reporte: "+fullDate+"\n\n");
             
             documento.open();
             documento.add(header);
@@ -146,6 +164,7 @@ public class ConInformes {
                 int ventaEfectivo = 0;
                 int ventaDebito = 0;
                 int ventaCredito = 0;
+                
                 for(Venta v: listado){
                     totalVenta += v.getTotal();
                     if(v.getTipo_venta().equals("EFECTIVO")) ventaEfectivo+=v.getTotal();
@@ -173,11 +192,133 @@ public class ConInformes {
                 documento.add(venta);
                 venta = new Paragraph();
                 venta.setFont(FontFactory.getFont(FontFactory.TIMES_ROMAN, 12, Font.NORMAL, BaseColor.BLACK));
-                venta.add("CRÉDITO:   "+str_credito);
+                venta.add("CRÉDITO:   "+str_credito+"\n\n");
                 documento.add(venta);
+                
+                ArrayList<Object[]> resumenMetodoPago = cVenta.resumenVentaMetodoPago(fecha);
+                
+
+
+                
+                PdfPTable tablaEfectivo = new PdfPTable(new float[]{10,20, 30,20});
+                PdfPTable tablaDebito = new PdfPTable(new float[]{10,20, 30,20});
+                PdfPTable tablaCredito = new PdfPTable(new float[]{10,20, 30,20});
+                PdfPCell cell = new PdfPCell();
+                cell.setBackgroundColor(BaseColor.GRAY);
+                Font font = new Font(Font.FontFamily.HELVETICA, 10, Font.BOLD);
+                font.setColor(BaseColor.WHITE);
+                cell.setPhrase(new Phrase("FOLIO", font));
+                tablaEfectivo.addCell(cell);
+                tablaDebito.addCell(cell);
+                tablaCredito.addCell(cell);                
+                cell.setPhrase(new Phrase("METODO PAGO", font));
+                tablaEfectivo.addCell(cell);
+                tablaDebito.addCell(cell);
+                tablaCredito.addCell(cell); 
+                cell.setPhrase(new Phrase("FECHA", font));
+                tablaEfectivo.addCell(cell);
+                tablaDebito.addCell(cell);
+                tablaCredito.addCell(cell); 
+                cell.setPhrase(new Phrase("TOTAL", font));
+                tablaEfectivo.addCell(cell);
+                tablaDebito.addCell(cell);
+                tablaCredito.addCell(cell); 
+                
+                for(Object[] o: resumenMetodoPago){
+                    int int_folio = (int) o[0];
+                    String str_folio = String.valueOf(int_folio);
+                    String tipo_venta = o[1].toString();
+                    String fechaTransacction = o[2].toString();
+                    int int_total = (int) o[3];
+                    String str_total = String.format("$%,d",Integer.valueOf(int_total));
+                    PdfPCell cellTotal = new PdfPCell();
+                    cellTotal.setHorizontalAlignment(Chunk.ALIGN_RIGHT);
+                    cellTotal.setPhrase(new Phrase(str_total));
+                    
+                    if(tipo_venta.equals("EFECTIVO")){
+                        tablaEfectivo.addCell(str_folio);
+                        tablaEfectivo.addCell(tipo_venta);
+                        tablaEfectivo.addCell(fechaTransacction);
+                        tablaEfectivo.addCell(cellTotal);   
+                    }
+                    else if(tipo_venta.equals("DEBITO")){
+                        tablaDebito.addCell(str_folio);
+                        tablaDebito.addCell(tipo_venta);
+                        tablaDebito.addCell(fechaTransacction);
+                        tablaDebito.addCell(cellTotal); 
+                    }
+                    else{
+                        tablaCredito.addCell(str_folio);
+                        tablaCredito.addCell(tipo_venta);
+                        tablaCredito.addCell(fechaTransacction);
+                        tablaCredito.addCell(cellTotal);                        
+                    }
+                    
+                }
+                Paragraph tituloEfectivo = new Paragraph();
+                tituloEfectivo.setAlignment(Chunk.ALIGN_CENTER);
+                tituloEfectivo.setFont(FontFactory.getFont(FontFactory.TIMES_ROMAN, 12, Font.NORMAL, BaseColor.BLUE));
+                tituloEfectivo.add("METODO DE PAGO: EFECTIVO\n\n");
+                documento.add(tituloEfectivo);
+                documento.add(tablaEfectivo);
+                
+                
+                Paragraph tituloDebito = new Paragraph();
+                tituloDebito.setAlignment(Chunk.ALIGN_CENTER);
+                tituloDebito.setFont(FontFactory.getFont(FontFactory.TIMES_ROMAN, 12, Font.NORMAL, BaseColor.BLUE));
+                tituloDebito.add("\n\nMETODO DE PAGO: DEBITO\n\n");
+                documento.add(tituloDebito);
+                documento.add(tablaDebito);
+                
+                Paragraph tituloCredito = new Paragraph();
+                tituloCredito.setAlignment(Chunk.ALIGN_CENTER);
+                tituloCredito.setFont(FontFactory.getFont(FontFactory.TIMES_ROMAN, 12, Font.NORMAL, BaseColor.BLUE));
+                tituloCredito.add("\n\nMETODO DE PAGO: CREDITO\n\n");
+                documento.add(tituloCredito);
+                documento.add(tablaCredito); 
+                
+                Paragraph tituloProducto = new Paragraph();
+                tituloProducto.setAlignment(Chunk.ALIGN_CENTER);
+                tituloProducto.setFont(FontFactory.getFont(FontFactory.TIMES_ROMAN, 12, Font.NORMAL, BaseColor.BLUE));
+                tituloProducto.add("\n\n\n\nDETALLE DE PRODUCTOS VENDIDOS\n\n");
+                
+                ArrayList<Object[]> detalleProducto = cVenta.detalleProductoVendido(fecha);
+                PdfPTable tablaProductos = new PdfPTable(new float[]{50,20,20});
+
+                cell.setPhrase(new Phrase("PRODUCTO", font));
+                tablaProductos.addCell(cell);               
+                cell.setPhrase(new Phrase("CANTIDAD", font));
+                tablaProductos.addCell(cell);
+                cell.setPhrase(new Phrase("TOTAL VENTA", font));
+                tablaProductos.addCell(cell);
+                
+                for(Object[] o: detalleProducto){
+                    String nombreProducto = o[0].toString();
+                    int cantidad = (int) o[1];
+                    int total = (int) o[2];
+                    String str_total = String.format("$%,d",Integer.valueOf(total));
+                    
+                    
+                    PdfPCell cellDerecha = new PdfPCell();
+                    cellDerecha.setHorizontalAlignment(Chunk.ALIGN_RIGHT);
+                    
+                    tablaProductos.addCell(nombreProducto);
+                    
+                    
+                    cellDerecha.setPhrase(new Phrase(String.valueOf(cantidad)));
+                    tablaProductos.addCell(cellDerecha);
+                    
+                    cellDerecha.setPhrase(new Phrase(str_total));
+                    tablaProductos.addCell(cellDerecha);
+                    
+                }
+                documento.add(tituloProducto);
+                documento.add(tablaProductos);
                 
                 
             }
+            
+            
             
             
             
--- src/controlador/ConVenta.java
+++ src/controlador/ConVenta.java
@@ -64,13 +64,71 @@ public class ConVenta {
                 v.setTipo_venta(rs.getString(3));
                 v.setTotal(rs.getInt(4));
                 lista.add(v);
+            }            
+        } 
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return lista;
+    }
+    
+    public ArrayList<Object[]> resumenVentaMetodoPago(String fecha){
+        ArrayList<Object[]> detalle = new ArrayList<>();
+        try {
+            Statement stmt = CONNECTION.createStatement();
+            //RESUMEN DE VENTAS SEGUN METODO DE PAGO
+            String query = "SELECT V.folio, V.tipo_venta, DATE_FORMAT(V.fecha, '%d/%m/%Y %H:%i:%s') as FECHA, SUM(VP.valor_total) " +
+                            "FROM VENTA V JOIN VENTA_PRODUCTO VP ON V.folio = VP.folio " +
+                            "JOIN PRODUCTO P ON VP.codigo_barra = P.codigo_barra " +
+                            "WHERE fecha BETWEEN '"+fecha+" 00:00:00' AND '"+fecha+" 23:59:59' " +
+                            "GROUP BY V.folio";
+            ResultSet rs = stmt.executeQuery(query);
+            while(rs.next()){
+                Object[] item = {
+                            rs.getInt(1),
+                            rs.getString(2),
+                            rs.getString(3),
+                            rs.getInt(4)
+                };
+                
+                detalle.add(item);                        
             }
             
         } 
         catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return lista;
+               
+        return detalle;
+    }
+    
+    public ArrayList<Object[]> detalleProductoVendido(String fecha){
+        ArrayList<Object[]> detalle = new ArrayList<>();
+        try {
+            Statement stmt = CONNECTION.createStatement();
+            //RESUMEN DE VENTAS SEGUN METODO DE PAGO
+            String query = "SELECT P.nombre, sum(VP.cantidad), sum(VP.valor_total) " +
+                            "FROM VENTA V JOIN VENTA_PRODUCTO VP ON V.folio = VP.folio " +
+                            "JOIN PRODUCTO P ON VP.codigo_barra = P.codigo_barra " +
+                            "WHERE fecha BETWEEN '"+fecha+" 00:00:00' AND '"+fecha+" 23:59:59' " +
+                            "GROUP BY P.nombre;";
+            ResultSet rs = stmt.executeQuery(query);
+            while(rs.next()){
+                Object[] item = {
+                            rs.getString(1),
+                            rs.getInt(2),
+                            rs.getInt(3)
+                };
+                
+                detalle.add(item);                        
+            }
+            
+        } 
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+               
+        return detalle;
     }
     
     
